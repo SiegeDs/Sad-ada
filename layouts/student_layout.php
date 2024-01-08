c219b3dc5241fefafba6fd3da3fdfc58e81f4a90
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2024-01-08 09:26:43 +0800

    Implement Account CRUD

--- layouts/student_layout.php
+++ layouts/student_layout.php
@@ -20,7 +20,7 @@
                         SYSTEM <img src="./images/snowflake.png" width="20" />
                     </div>
                     <nav class="nav m-2">
-                        <a class="nav-link <?= (($title == 'Manage Account') ? 'active text-warning' : "text-light") ?>" aria-current="page" href="manage_acc.php">
+                        <a class="nav-link <?= (($title == 'Manage Accounts') ? 'active text-warning' : "text-light") ?>" aria-current="page" href="manage_acc.php">
                             Manage Account
                         </a>
                         <a class="nav-link <?= (($title == 'Newsfeed') ? 'active text-warning' : "text-light") ?>" href="newsfeed.php">
@@ -125,9 +125,10 @@
             </div>
         </div>
     </nav>
-    <?php echo $content; ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <?php echo $content; ?>
     <script>
         function updateDateTime() {
             var currentDate = new Date();
